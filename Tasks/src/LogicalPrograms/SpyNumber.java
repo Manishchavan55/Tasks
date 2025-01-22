357fa8e4a2c1b7c9c4856241a86e1d68bcd6b386
Saurabh chavan <manishff87@gmail.com>
2025-01-22 18:39:30 +0530

    spy number check logic added....

--- Tasks/src/LogicalPrograms/SpyNumber.java
+++ Tasks/src/LogicalPrograms/SpyNumber.java
@@ -5,12 +5,17 @@ import java.util.Scanner;
 public class SpyNumber {
 	public static void main(String[] args) {
 		//The spy number is a number that each digit number addition and multiplication is same...!
-		int sum
+		int sum=0;
+		int mul=1;
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter the number");
 		int num=sc.nextInt();
 		while(num>0) {
 			 sum=sum+num%10;
+			mul*=num%10;
+			num=num/10;
 		}
+		
+		
 	}
 }
